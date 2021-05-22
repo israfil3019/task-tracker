c8f435c311f56083bf1da03715820db106d6b8b4
israfil <israfil3019@gmail.com>
2021-05-22 19:22:12 +0300

    add addTask form component

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddTask = () => {
+  const [text, setText] = useState("");
+  const [day, setDay] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(text);
+    console.log(day);
+  };
+
   return (
     <div>
-      <h2>add task</h2>
+      <form className="add-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="task">Task</label>
+          <input
+            id="task"
+            name="text"
+            type="text"
+            placeholder="Add Task"
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="day">Day & Time</label>
+          <input
+            id="day"
+            name="day"
+            type="text"
+            placeholder="Add Day & Time"
+            onChange={(e) => setDay(e.target.value)}
+          />
+        </div>
+        <input type="submit" value="Save Task" className="btn btn-block" />
+      </form>
     </div>
   );
 };
