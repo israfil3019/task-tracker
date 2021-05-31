02a4f10fcdb95186f0fd36dd321c9118eef07cb4
israfil <israfil3019@gmail.com>
2021-05-31 18:16:14 +0300

    create database with json-server

--- src/App.js
+++ src/App.js
@@ -1,55 +1,87 @@
-import "./App.css";
-import AddTask from "./components/AddTask";
-import Header from "./components/Header";
-import Tasks from "./components/Tasks";
-import { useState } from "react";
+import { useState, useEffect } from 'react';
+import './App.css';
+import AddTask from './components/AddTask';
+import Header from './components/Header';
+import Tasks from './components/Tasks';
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      text: "Study React Pre-Class Notes",
-      day: "Feb 5th at 2:30pm",
-      isDone: false,
-    },
-    {
-      id: 2,
-      text: "Feed the Dog",
-      day: "Feb 6th at 1:30pm",
-      isDone: false,
-    },
-    {
-      id: 3,
-      text: "Attend in-Class",
-      day: "Feb 7th at 20:00pm",
-      isDone: false,
-    },
-  ]);
-
+  const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 100) + 1;
-    const newTask = { id, ...task };
-    setTasks([...tasks, newTask]);
+  const baseUrl = 'http://localhost:5000/tasks';
+
+  //Fetch tasks
+  const fetchTasks = async () => {
+    const res = await fetch(baseUrl);
+    const data = await res.json();
+    console.log(data);
+    setTasks(data);
   };
 
-  const deleteTask = (deletedTaskId) => {
-    setTasks(tasks.filter((task) => task.id !== deletedTaskId));
+  useEffect(() => {
+    fetchTasks();
+  }, []);
+
+  // Add Task
+  const addTask = async (task) => {
+    const res = await fetch(baseUrl, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(task),
+    });
+    const data = await res.json();
+    fetchTasks();
   };
 
-  const toggleDone = (toggleDoneId) => {
-    setTasks(
-      tasks.map((task) =>
-        task.id === toggleDoneId ? { ...task, isDone: !task.isDone } : task
-      )
-    );
+  // const addTask = (task) => {
+  //   const id = Math.floor(Math.random() * 100) + 1;
+  //   const newTask = { id, ...task };
+  //   setTasks([...tasks, newTask]);
+  // };
+
+  // Delete Task
+  const deleteTask = async (deletedTaskId) => {
+    await fetch(`${baseUrl}/${deletedTaskId}`, {
+      method: 'DELETE',
+    });
+    fetchTasks();
   };
 
+  // const deleteTask = (deletedTaskId) => {
+  //   setTasks(tasks.filter((task) => task.id !== deletedTaskId));
+  // };
+
+  // Toggle Done
+  const toggleDone = async (toggleDoneId) => {
+    const res = await fetch(`${baseUrl}/${toggleDoneId}`);
+    const data = await res.json();
+    const updatedTask = { ...data, isDone: !data.isDone };
+
+    await fetch(`${baseUrl}/${toggleDoneId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(updatedTask),
+    });
+    fetchTasks();
+  };
+
+  // const toggleDone = (toggleDoneId) => {
+  //   setTasks(
+  //     tasks.map((task) =>
+  //       task.id === toggleDoneId ? { ...task, isDone: !task.isDone } : task
+  //     )
+  //   );
+  // };
+
+  // Toggle Show
   const toggleShow = () => setShowAddTask(!showAddTask);
 
   return (
-    <div className="App">
+    <div className="container">
       <Header
         title="Task Tracker"
         showAddTask={showAddTask}
@@ -59,7 +91,7 @@ function App() {
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} deleteTask={deleteTask} toggleDone={toggleDone} />
       ) : (
-        "No Tasks to Show"
+        'No Tasks to Show'
       )}
     </div>
   );
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,44 +1,69 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from 'react';
+//import serialize from 'form-serialize';
 
 const AddTask = ({ addTask }) => {
-  const [text, setText] = useState("");
-  const [day, setDay] = useState("");
+  const [text, setText] = useState('');
+  const [day, setDay] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleTextChange = (e) => setText(e.target.value);
+  const handleDayChange = (e) => setDay(e.target.value);
+
+  // const text = useRef();
+  // const day = useRef();
+
+  const onSubmit = (e) => {
     e.preventDefault();
-    addTask({ text, day, isDone: false });
-    setText("");
-    setDay("");
+
+    //form-serialize
+    // var { text, day } = serialize(e.target, { hash: true });
+
+    !text ? alert('Please add a task') : addTask({ text, day, isDone: false });
+
+    setText('');
+    setDay('');
+
+    // with useRef
+    // !text.current.value
+    //   ? alert('Please add a task')
+    //   : addTask({
+    //       text: text.current.value,
+    //       day: day.current.value,
+    //       isDone: false,
+    //     });
+
+    // text.current.value = '';
+    // day.current.value = '';
   };
 
   return (
-    <div>
-      <form className="add-form" onSubmit={handleSubmit}>
-        <div className="form-control">
-          <label htmlFor="task">Task</label>
-          <input
-            id="task"
-            name="text"
-            type="text"
-            placeholder="Add Task"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
-          />
-        </div>
-        <div className="form-control">
-          <label htmlFor="day">Day & Time</label>
-          <input
-            id="day"
-            name="day"
-            type="text"
-            placeholder="Add Day & Time"
-            value={day}
-            onChange={(e) => setDay(e.target.value)}
-          />
-        </div>
-        <input type="submit" value="Save Task" className="btn btn-block" />
-      </form>
-    </div>
+    <form className="add-form" onSubmit={onSubmit}>
+      <div className="form-control">
+        <label htmlFor="task">Task</label>
+        <input
+          id="task"
+          name="text"
+          type="text"
+          placeholder="AddTask"
+          //ref={text}
+          required
+          value={text}
+          onChange={handleTextChange}
+        />
+      </div>
+      <div className="form-control">
+        <label htmlFor="day">Day & Time</label>
+        <input
+          id="day"
+          name="day"
+          type="text"
+          placeholder="Add Day & Time"
+          //ref={day}
+          value={day}
+          onChange={handleDayChange}
+        />
+      </div>
+      <input type="submit" value="Save Task" className="btn btn-block" />
+    </form>
   );
 };
 
